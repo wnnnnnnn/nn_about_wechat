ded4b1067eb070e91fae9b69dc5845a70814c066
wnnnnnn <1152917824@qq.com>
2019-05-24 09:57:35 +0800

    test

---
+++ demo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mr wang
+ * Date: 2019-05-24
+ * Time: 9:50
+ */
+require "./vendor/autoload.php";
+
+use Wnaboutwechat\wechatLogin\Login;
+
+Login::Login();
---
+++ src/Wnaboutwechat/wechatLogin/Login.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mr wang
+ * Date: 2019-05-24
+ * Time: 9:45
+ */
+namespace Wnaboutwechat\wechatLogin;
+
+class Login{
+    public static function Login(){
+        echo 'Hello world';
+    }
+}
