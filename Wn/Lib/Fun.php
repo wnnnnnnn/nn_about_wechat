d93bfab79ec460496e7a161b06f9d1f4424b84b5
wnnnnnn <1152917824@qq.com>
2019-05-27 11:07:28 +0800

    aaa

--- Wn/Lib/Fun.php
+++ Wn/Lib/Fun.php
@@ -8,14 +8,15 @@
 namespace Wn\Lib;
 
 class Fun{
-    public static function url_connect($url,...$param){
+    public static function url_connect($url,$param){
         if(count($param)<1){
             return $url;
         }
         foreach($param as $key => $value){
             $key == 0 ? $sign = '?' : $sign = '&';
             $url .= $sign.$value['key'].'='.$value['v'];
-            return $url;
+
         }
+        return $url;
     }
 }
